90be165b5f4a908cfd261a7b74f783af6661cb62
Aki158 <xiaolinh548@gmail.com>
2024-12-02 23:07:10 +0900

    #167_numberOfDotsファイルを追加

---
+++ #167_numberOfDots.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int numberOfDots(int x){
+    if(x == 0){
+        return 0;
+    }
+    return x + numberOfDots(x-1);
+}
